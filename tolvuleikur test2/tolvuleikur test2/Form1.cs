31c885974780557f6abaa158eeac492d627551a0
andrisnrb <andri.brynjarsson@gmail.com>
2016-11-22 10:04:20 +0000

    Update Form1.cs

--- tolvuleikur test2/tolvuleikur test2/Form1.cs
+++ tolvuleikur test2/tolvuleikur test2/Form1.cs
@@ -122,19 +122,41 @@ namespace tolvuleikur_test2
             }
         }
 
-        //Hús
+       //Hús
         public void Hús1(int x, int y)
         {
-
+            for (int i = 0; i < World.Length; i++)
+            {
+                if (World[i].Name == x + ":" + y ||  World[i].Name == (x + 4) + ":" + y || 
+                    World[i].Name == x + ":" + (y + 1) || World[i].Name == x + ":" + (y + 2) || World[i].Name == x + ":" + (y + 3) || World[i].Name == x + ":" + (y + 4) || World[i].Name == x + ":" + (y+5) ||
+                    World[i].Name == (x + 4) + ":" + (y + 1) || World[i].Name == (x + 4) + ":" + (y + 2) || World[i].Name == (x + 4) + ":" + (y + 3) || World[i].Name == (x + 4) + ":" + (y + 4) || World[i].Name == (x + 4) + ":" + (y + 5))
+                {
+                   
+                    World[i].BackColor = Color.FromArgb(69, 36, 14);
+                }
+                if (World[i].Name == (x + 1) + ":" + y || World[i].Name == (x + 3) + ":" + y ||
+                    World[i].Name == (x + 1) + ":" + (y + 1) || World[i].Name == (x + 1) + ":" + (y + 2) || World[i].Name == (x + 1) + ":" + (y + 3) || World[i].Name == (x + 1) + ":" + (y + 4) || World[i].Name == (x + 1) + ":" + (y + 5) ||
+                    World[i].Name == (x + 3) + ":" + (y + 1) || World[i].Name == (x + 3) + ":" + (y + 2) || World[i].Name == (x + 3) + ":" + (y + 3) || World[i].Name == (x + 3) + ":" + (y + 4) || World[i].Name == (x + 3) + ":" + (y + 5))
+	            {
+		            World[i].BackColor = Color.FromArgb(78, 47, 18);
+	            }
+                if (World[i].Name == (x + 2) + ":" + y || 
+                    World[i].Name == (x + 2) + ":" + (y + 1) || World[i].Name == (x + 2) + ":" + (y + 2) || World[i].Name == (x + 2) + ":" + (y + 3) || World[i].Name == (x + 2) + ":" + (y + 4) || World[i].Name == (x + 2) + ":" + (y + 5))
+	            {
+                    World[i].BackColor = Color.FromArgb(86, 51, 20);
+	            }
+            }
+      
         }
         public void Hús2(int x, int y)
         {
-
+            for (int i = 0; i < World.Length; i++)
+            {
+                if (World[i].Name == x + ":" + y || World[i].Name == (x+1) + ":" + y || World[i].Name == (x+2) + ":" + y || World[i].Name == (x+3) + ":" + y || World[i].Name == (x+4) + ":" + y || World[i].Name == (x+5) + ":" + y ||
+                    World[i].Name == x + ":" + (y+4) || World[i].Name == (x + 1) + ":" + (y+4) || World[i].Name == (x + 2) + ":" + (y+4) || World[i].Name == (x + 3) + ":" + (y+4) || World[i].Name == (x + 4) + ":" + (y+4) || World[i].Name == (x + 5) + ":" + (y+4))
+                {
+                
+                }
+            
+            }
         }
-        public void Hús3(int x, int y)
-        {
-
-        }
-
-    }
-}
